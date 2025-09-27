ea37891d45b268a59b5d9e27ba1c6b4961170047
tugascrown204 <gajdertevet@hotmail.com>
2025-09-27 15:52:54 +0330

    feat: initial commit for python/example_template.py

---
+++ python/example_template.py
@@ -0,0 +1,11 @@
+def greet(name):
+    """
+    Greet a person with a friendly message.
+    
+    Args:
+        name (str): The name of the person to greet.
+    """
+    return f"Hello, {name}!"
+
+if __name__ == '__main__':
+    print(greet('World'))
