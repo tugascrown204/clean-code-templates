a68734a13d947227d810a550bd52becc9893a544
tugascrown204 <gajdertevet@hotmail.com>
2025-09-27 15:52:55 +0330

    feat: initial commit for javascript/example_template.js

---
+++ javascript/example_template.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    /**
+     * Greet a person with a friendly message.
+     *
+     * @param {string} name - The name of the person to greet.
+     * @returns {string} Greeting message.
+     */
+    return `Hello, ${name}!`;
+}
+
+console.log(greet('World'));
